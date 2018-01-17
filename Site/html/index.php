8d467db9659bc9f7342172230f500c1b35f473c2
Quentin Gans <quentin.gans@outlook.fr>
2018-01-17 10:28:01 +0100

    Effet Fondus <3

--- Site/html/index.php
+++ Site/html/index.php
@@ -112,6 +112,16 @@
             })
         }, 3500);
     });
+
+    // Effet Fondus
+    setInterval(Frames,50);
+    function Frames(){
+        var pos = $(".slideshow ul").find(":eq(3)").position();
+        pos.left = Math.abs(390-Math.abs(300-pos.left))/370;
+        console.log(pos.left);
+        $(".slideshow ul").find(":eq(6)").css("opacity",pos.left);
+    };
+
 </script>
 
 </html>
