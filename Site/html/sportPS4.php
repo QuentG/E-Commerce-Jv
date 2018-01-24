97920bca28ddcd8a95d43f439205202cc9bd2ea3
Unknown <florian.borie@ynov.com>
2018-01-24 15:27:09 +0100

    page ps4 sport ok et les degue de mes pages

--- Site/html/sportPS4.php
+++ Site/html/sportPS4.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>PS4 sport: GamingKeys</title>
-    <link type="text/css" rel="stylesheet" href="../css/PS4.css"/>
+    <link type="text/css" rel="stylesheet" href="../css/sportPS4.css"/>
     <link type="text/css" rel="stylesheet" href="../css/style.css"/>
     <link type="text/css" rel="stylesheet" href="../css/reset.css"/>
     <link type="text/php" href="../html/recup.php"/>
@@ -75,22 +75,30 @@
     </ul>
 </div>
 <div>
-    <h1 id="titre_slider">Top 10 des ventes du moment sur PS4</h1>
-    <div class="slideshow">
-        <ul>
-            <!--top des jeux PS4-->
-            <li><img class="image_slider" src="../img/ps4/cod-wwii-ps4-front-en_2000x.png" alt="image du jeux Call of duty World War II sur PS4"/></li>
-            <li><img class="image_slider" src="../img/ps4/jaquette-grand-theft-auto-v-playstation-4-ps4-cover-avant-g-1415122088.jpg" alt="image du jeux grand theft auto v sur PS4"/></li>
-            <li><img class="image_slider" src="../img/ps4/166433-ps4-gtsport-standard.png" alt="image du jeux gran turismo sport sur PS4"/></li>
-            <li><img class="image_slider" src="../img/ps4/161376.jpg" alt="image du jeux Wolfenstein II : The New Colossus sur PS4"/></li>
-            <li><img class="image_slider" src="../img/ps4/C0R4myqWgAERMF2__92896_zoom.jpg" alt="image du jeux  Crash Bandicoot N. Sane Trilogy sur PS4"/></li>
-            <li><img class="image_slider" src="../img/ps4/cover_large.jpg" alt="image du jeux Full Throttle Remastered sur PS4"/></li>
-            <li><img class="image_slider" src="../img/ps4/shadow-tactics-blades-of-the-shogun-ps4-1511897711.jpg" alt="image du jeux Shadow Tactics : Blades of the Shogun sur PS4"/></li>
-            <li><img class="image_slider" src="../img/ps4/téléchargement (1).jpg" alt="image  du jeux Hellblade sur PS4"/></li>
-            <li><img class="image_slider" src="../img/ps4/téléchargement.jpg" alt="image du jeux Yakuza Kiwami sur PS4"/></li>
-            <li><img class="image_slider" src="../img/ps4/ud340_d827da85.jpg" alt="image du jeux Final Fantasy XII : The Zodiac Age sur PS4"/></li>
-        </ul>
-    </div>
+    <img class="image" src="../img/ps4/sport/817lbwb+4FL._SL1500_.jpg" alt="image du jeux Assetto Corsa sur PS4"/>
+    <img class="image1" src="../img/ps4/sport/blood-bowl-2-jaquette-55fbf0bc68d37.jpg" alt="image du jeux Blood Bowl II sur PS4"/>
+    <img class="image1" src="../img/ps4/sport/NewMinGol_PS4_Front_EN_noRating-e1494657927648.jpg" alt="image du jeux Everybody's Golf sur PS4"/>
+    <img class="image1" src="../img/ps4/sport/téléchargement (3).jpg" alt="image du jeux fifa 18 sur PS4"/>
+    <img class="image" src="../img/ps4/sport/16117-fifa-17.jpg" alt="image du jeux fifa 17 sur PS4"/>
+    <img class="image1" src="../img/ps4/sport/166433-ps4-gtsport-standard.png" alt="image du jeux Gran Turismo Sport sur PS4"/>
+    <img class="image1" src="../img/ps4/sport/madden-18-ps4-fr-new.jpg" alt="image du jeux Madden 18 sur PS4"/>
+    <img class="image1" src="../img/ps4/sport/23a6d715-020d-430f-8edf-522f0cb9b710_1.22cf63e5e280fa480ddd2a4fcc078ae5.jpeg" alt="image du jeux Madden 17 sur PS4"/>
+    <img class="image" src="../img/ps4/sport/jx1001803202@2x.jpg" alt="image du jeux nba 2018 sur PS4"/>
+    <img class="image1" src="../img/ps4/sport/1467795080-3062-jaquette-avant.jpg" alt="image du jeux nba 2017 sur PS4"/>
+    <img class="image1" src="../img/ps4/sport/3max.jpg" alt="image du jeux nhl 15 sur PS4"/>
+    <img class="image1" src="../img/ps4/sport/71HpqCd3qaL._SL1000_.jpg" alt="image du jeux nhl 16 sur PS4"/>
+    <img class="image" src="../img/ps4/sport/ps4-pro-evolution-soccer-2018-premium-edition-pes-2018-r2-pre-order-gr31-1708-30-F514465_1.jpg" alt="image du jeux pes 2018 sur PS4"/>
+    <img class="image1" src="../img/ps4/sport/716CecV6VVL._SL1000_.jpg" alt="image du jeux pes 2015 sur PS4"/>
+    <img class="image1" src="../img/ps4/sport/téléchargement.jpg" alt="image du jeux NBA Playground sur PS4"/>
+    <img class="image1" src="../img/ps4/sport/rocketleague.jpg" alt="image du jeux rocket league sur PS4"/>
+    <img class="image" src="../img/ps4/sport/91F+Kh-MzHL._SL1500_.jpg" alt="image du jeux rugby 15 sur PS4"/>
+    <img class="image1" src="../img/ps4/sport/rc3_aus_ps4.jpg" alt="image du jeux Rugby Challenge 3 sur PS4"/>
+    <img class="image1" src="../img/ps4/sport/PS4ST57.jpg" alt="image du jeux steep sur PS4"/>
+    <img class="image1" src="../img/ps4/sport/71BbbmK+UKL._SL1000_.jpg" alt="image du jeux Tony Hawk's Pro Skater 5 sur PS4"/>
+    <img id="image1" src="../img/ps4/sport/193883822.jpeg" alt="image du jeux ufc 3 sur PS4"/>
+    <img class="image2" src="../img/ps4/sport/71i634KZ0+L._SL1000_.jpg" alt="image du jeux ufc 2 sur PS4"/>
+    <img class="image2" src="../img/ps4/sport/1505306395-6137-jaquette-avant.jpg" alt="image du jeux w2018 sur PS4"/>
+    <img class="image2" src="../img/ps4/sport/91v9bNfrtYL._SL1500_.jpg" alt="image du jeux w2017 sur PS4"/>
 </div>
 
 
