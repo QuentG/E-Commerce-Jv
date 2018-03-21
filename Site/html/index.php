8ffa1ecdd3fc93b7b9c0a56bbdad7e8c80c41a1f
Unknown <florian.borie@ynov.com>
2018-03-21 14:21:10 +0100

    mise en place reponsive slider

--- Site/html/index.php
+++ Site/html/index.php
@@ -25,80 +25,115 @@ session_start();
 </head>
 
 <body>
-    <!--nav bar-->
-    <nav class="navbar navbar-inverse">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#main-menu">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand hidden-md hidden-lg" href="#">Menu</a>
-        </div>
+<!--nav bar-->
+<nav class="navbar navbar-inverse">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#main-menu">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand hidden-md hidden-lg" href="#">Menu</a>
+    </div>
 
-        <div class="collapse navbar-collapse" id="main-menu" style="margin-bottom: 0px;">
-            <ul class="nav navbar-nav">
-                <li class="dropdown">
-                    <a data-toggle="dropdown" href="../html/index.php">Accueil<b></b></a>
-                </li>
-                <li class="dropdown">
-                    <a data-toggle="dropdown" href="#">PC<b class="caret"></b></a>
-                    <ul class="dropdown-menu jqueryFadeIn">
-                        <li><a href="../html/index.php">Accueil</a> </li>
-                        <li><a href="../html/PC.php">Accueil PC</a></li>
-                        <li><a href="../html/aventurePC.php">Aventure</a></li>
-                        <li><a href="../html/actionPC.php">Action</a></li>
-                        <li><a href="../html/combatPC.php">Combat</a></li>
-                        <li><a href="../html/sportPC.php">Sport</a></li>
-                    </ul>
-                </li>
+    <div class="collapse navbar-collapse" id="main-menu" style="margin-bottom: 0px;">
+        <ul class="nav navbar-nav">
+            <li class="dropdown">
+                <a data-toggle="dropdown" href="../html/index.php">Accueil<b></b></a>
+            </li>
+            <li class="dropdown">
+                <a data-toggle="dropdown" href="#">PC<b class="caret"></b></a>
+                <ul class="dropdown-menu jqueryFadeIn">
+                    <li><a href="../html/index.php">Accueil</a> </li>
+                    <li><a href="../html/PC.php">Accueil PC</a></li>
+                    <li><a href="../html/aventurePC.php">Aventure</a></li>
+                    <li><a href="../html/actionPC.php">Action</a></li>
+                    <li><a href="../html/combatPC.php">Combat</a></li>
+                    <li><a href="../html/sportPC.php">Sport</a></li>
+                </ul>
+            </li>
 
-                <li class="dropdown">
-                    <a data-toggle="dropdown" href="#">PS4<b class="caret"></b></a>
-                    <ul class="dropdown-menu jqueryFadeIn">
-                        <li><a href="../html/index.php">Accueil</a> </li>
-                        <li><a href="../html/PS4.php">Accueil PS4</a></li>
-                        <li><a href="../html/aventurePS4.php">Aventure</a></li>
-                        <li><a href="../html/actionPS4.php">Action</a></li>
-                        <li><a href="../html/combatPS4.php">Combat</a></li>
-                        <li><a href="../html/sportPS4.php">Sport</a></li>
-                    </ul>
-                </li>
+            <li class="dropdown">
+                <a data-toggle="dropdown" href="#">PS4<b class="caret"></b></a>
+                <ul class="dropdown-menu jqueryFadeIn">
+                    <li><a href="../html/index.php">Accueil</a> </li>
+                    <li><a href="../html/PS4.php">Accueil PS4</a></li>
+                    <li><a href="../html/aventurePS4.php">Aventure</a></li>
+                    <li><a href="../html/actionPS4.php">Action</a></li>
+                    <li><a href="../html/combatPS4.php">Combat</a></li>
+                    <li><a href="../html/sportPS4.php">Sport</a></li>
+                </ul>
+            </li>
 
-                <li class="dropdown">
-                    <a data-toggle="dropdown" href="#">XBOX ONE<b class="caret"></b></a>
-                    <ul class="dropdown-menu jqueryFadeIn">
-                        <li><a href="../html/index.php">Accueil</a> </li>
-                        <li><a href="../html/XboxONE.php">Accueil XBOX ONE</a></li>
-                        <li><a href="../html/aventureXboxOne.php">Aventure</a></li>
-                        <li><a href="../html/actionXboxOne.php">Action</a></li>
-                        <li><a href="../html/combatXboxOne.php">Combat</a></li>
-                        <li><a href="../html/sportXboxOne.php">Sport</a></li>
-                    </ul>
-                </li>
+            <li class="dropdown">
+                <a data-toggle="dropdown" href="#">XBOX ONE<b class="caret"></b></a>
+                <ul class="dropdown-menu jqueryFadeIn">
+                    <li><a href="../html/index.php">Accueil</a> </li>
+                    <li><a href="../html/XboxONE.php">Accueil XBOX ONE</a></li>
+                    <li><a href="../html/aventureXboxOne.php">Aventure</a></li>
+                    <li><a href="../html/actionXboxOne.php">Action</a></li>
+                    <li><a href="../html/combatXboxOne.php">Combat</a></li>
+                    <li><a href="../html/sportXboxOne.php">Sport</a></li>
+                </ul>
+            </li>
 
-            </ul>
-            <a href="../html/index.php" title="liens vers la page d'accueil"> <img id="logo" src="../img/Logo.png" alt="image du logo du site"/></a>
+        </ul>
+        <a href="../html/index.php" title="liens vers la page d'accueil"> <img id="logo" src="../img/Logo.png" alt="image du logo du site"/></a>
 
-            <form class="navbar-form pull-right">
-                <input id="recherche" type="text" style="width:150px"  placeholder="Recherche">
-                <button type="submit" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-search"></span> </button>
-            </form>
+        <form class="navbar-form pull-right">
+            <input id="recherche" type="text" style="width:150px"  placeholder="Recherche">
+            <button type="submit" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-search"></span> </button>
+        </form>
 
-            <form class="navbar-form pull-right">
-                <button id="btninsciption" type="button"> <a href="./inscription.php">Inscription </a></button>
-                <button id="btnconnexion" type="button"> <a href="./inscription.php"> Connexion </a> </button>
-            </form>
-        </div>
-    </nav>
+        <form class="navbar-form pull-right">
+            <button id="btninsciption" type="button"> <a href="./inscription.php">Inscription </a></button>
+            <button id="btnconnexion" type="button"> <a href="./inscription.php"> Connexion </a> </button>
+        </form>
+    </div>
+</nav>
 
-    <!--fin nav bar-->
+<!--fin nav bar-->
 
 <div class="jumbotron">
     <h1>GamingKeys !</h1>
     <p> Waw on va mettre du texte ça va être trop bien.</p>
 </div>
+<!--debut du reponsive à parir 1120px-->
+<div id="responsive">
+    <div class="titre_slider1">
+        <h2>top 3 des ventes du moment sur PC</h2>
+    </div>
+    <div class="image_jeux1">
+        <!--top des jeux pc-->
+        <img class="image_jeux" src="../img/pc/sport/91xtxVeMLHL._SL1500_.jpg" alt="image du jeux Fifa 18 sur PC"/>
+        <img class="image_jeux" src="../img/pc/action/jaquette-grand-theft-auto-v-pc-cover-avant-g-1415122060.jpg" alt="image du jeux Grand Theft Auto V sur PC"/>
+        <img class="image_jeux" src="../img/pc/action/9139QQmsbdL._SL1500.jpg" alt="image du jeux Star Wars Battlefront II sur PC"/>
+
+    </div>
+
+    <div class="titre_slider1">
+        <h2>top 3 des ventes du moment sur PS4</h2>
+    </div>
+    <div class="image_jeux1">
+        <!--top des jeux PS4-->
+        <img class="image_jeux" src="../img/ps4/action/cod-wwii-ps4-front-en_2000x.png" alt="image  du jeux Call of duty World War II sur PS4"/>
+        <img class="image_jeux" src="../img/ps4/action/jaquette-grand-theft-auto-v-playstation-4-ps4-cover-avant-g-1415122088.jpg" alt="image  du jeux grand theft auto v sur PS4"/>
+        <img class="image_jeux" src="../img/ps4/sport/166433-ps4-gtsport-standard.png" alt="image  du jeux gran turismo sport sur PS4"/>
+    </div>
+
+    <div class="titre_slider1">
+        <h2>top 3 des ventes du moment sur XBOX ONE</h2>
+    </div>
+    <div class="image_jeux1">
+        <!--top des jeux Xbox ONE-->
+        <img class="image_jeux" src="../img/xboxOne/action/2228.jpg" alt="image du jeux assassin creed origin sur Xbox One"/>
+        <img class="image_jeux" src="../img/xboxOne/action/108668b.jpg" alt="image du jeux halo 5 sur Xbox One"/>
+        <img class="image_jeux" src="../img/xboxOne/aventure/2227.jpg" alt="image du jeux la terre de milieu l'ombre de la guerre sur Xbox One"/>
+    </div>
+</div>
+<!--fin du reponsive à parir 1120px-->
+
 <!--titre slider-->
 <div id="fond">
     <h1 id="titre_slider">Top 3 des ventes du moment sur PC, PS4 & XBOX ONE</h1>
@@ -122,8 +157,7 @@ session_start();
     </div>
 </div>
 
-<?php include_once('./footer.php'); ?>
-
+<?php  include_once('./footer.php'); ?>
 
 </body>
 <script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
