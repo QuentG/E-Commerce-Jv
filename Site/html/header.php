fadccf74d0a1de0643ffbeec3c456a38434845a8
Quentin Gans <quentin.gans@outlook.fr>
2018-04-17 12:24:24 +0200

    Ajout System Connexion

--- Site/html/header.php
+++ Site/html/header.php
@@ -1,4 +1,11 @@
-<!--Nav bar-->
+<?php
+
+    if(isset($_SESSION['id']) AND !empty(isset($_SESSION['id'])))
+    {
+?>
+
+
+<!--Nav bar si la personne est connecté-->
 <nav class="navbar navbar-inverse">
     <div class="navbar-header">
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#main-menu">
@@ -49,7 +56,83 @@
             </li>
 
         </ul>
-        <a href="../html/index.php" title="liens vers la page d'accueil"> <img id="logo" src="../img/Logo.png" alt="image du logo du site"/></a>
+        <a href="../html/index.php" title="Vers la page d'accueil"> <img id="logo" src="../img/Logo.png" alt="Logo du site"/></a>
+
+        <form class="navbar-form pull-right">
+          <!--<p> Bienvenue, <?= $user['pseudo']; ?> </p> -->
+            <button id="btninsciption" type="button"> <a href="./profil.php">Profil </a></button>
+            <button id="btnconnexion" type="button"> <a href="./deconnexion.php"> Deconnexion </a> </button>
+        </form>
+    </div>
+</nav>
+
+<div class="jumbotron">
+    <h1>GamingKeys !</h1>
+    <p> Bienvenue sur GamingKeys.</p>
+</div>
+<!--Fin Nav bar-->
+
+
+
+        <?php
+
+         }else {
+
+        ?>
+
+
+<!--Nav bar si la personne n'est pas connecté-->
+<nav class="navbar navbar-inverse">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#main-menu">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand hidden-md hidden-lg" href="#">Menu</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="main-menu" style="margin-bottom: 0;">
+        <ul class="nav navbar-nav">
+            <li class="dropdown">
+                <a href="../html/index.php">Accueil<b></b></a>
+            </li>
+            <li class="dropdown">
+                <a data-toggle="dropdown" href="#">PC<b class="caret"></b></a>
+                <ul class="dropdown-menu jqueryFadeIn">
+                    <li><a href="../html/PC.php">Accueil PC</a></li>
+                    <li><a href="../html/aventurePC.php">Aventure</a></li>
+                    <li><a href="../html/actionPC.php">Action</a></li>
+                    <li><a href="../html/combatPC.php">Combat</a></li>
+                    <li><a href="../html/sportPC.php">Sport</a></li>
+                </ul>
+            </li>
+
+            <li class="dropdown">
+                <a data-toggle="dropdown" href="#">PS4<b class="caret"></b></a>
+                <ul class="dropdown-menu jqueryFadeIn">
+                    <li><a href="../html/PS4.php">Accueil PS4</a></li>
+                    <li><a href="../html/aventurePS4.php">Aventure</a></li>
+                    <li><a href="../html/actionPS4.php">Action</a></li>
+                    <li><a href="../html/combatPS4.php">Combat</a></li>
+                    <li><a href="../html/sportPS4.php">Sport</a></li>
+                </ul>
+            </li>
+
+            <li class="dropdown">
+                <a data-toggle="dropdown" href="#">XBOX ONE<b class="caret"></b></a>
+                <ul class="dropdown-menu jqueryFadeIn">
+                    <li><a href="../html/XboxONE.php">Accueil XBOX ONE</a></li>
+                    <li><a href="../html/aventureXboxOne.php">Aventure</a></li>
+                    <li><a href="../html/actionXboxOne.php">Action</a></li>
+                    <li><a href="../html/combatXboxOne.php">Combat</a></li>
+                    <li><a href="../html/sportXboxOne.php">Sport</a></li>
+                </ul>
+            </li>
+
+        </ul>
+        <a href="../html/index.php" title="Vers la page d'accueil"> <img id="logo" src="../img/Logo.png" alt="Logo du site"/></a>
 
 
         <form class="navbar-form pull-right">
@@ -66,6 +149,6 @@
 <!--Fin Nav bar-->
 
 
-
+<?php } ?>
 
 
