3919eb0f910af524ad7aa6547e1dc8a6dd7b4e34
Quentin Gans <quentin.gans@outlook.fr>
2018-04-17 18:04:13 +0200

    Hearder Repair

--- Site/html/header.php
+++ Site/html/header.php
@@ -26,10 +26,10 @@ if(isset($_SESSION['id']) AND !empty(isset($_SESSION['id'])))
                     <a data-toggle="dropdown" href="#">PC<b class="caret"></b></a>
                     <ul class="dropdown-menu jqueryFadeIn">
                         <li><a href="../html/PC.php">Accueil PC</a></li>
-                        <li><a href="../html/aventurePC.php">Aventure</a></li>
-                        <li><a href="../html/actionPC.php">Action</a></li>
-                        <li><a href="../html/combatPC.php">Combat</a></li>
-                        <li><a href="../html/sportPC.php">Sport</a></li>
+                        <li><a href="../html/jeux.php">Aventure</a></li>
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Combat</a></li>
+                        <li><a href="#">Sport</a></li>
                     </ul>
                 </li>
 
@@ -37,10 +37,10 @@ if(isset($_SESSION['id']) AND !empty(isset($_SESSION['id'])))
                     <a data-toggle="dropdown" href="#">PS4<b class="caret"></b></a>
                     <ul class="dropdown-menu jqueryFadeIn">
                         <li><a href="../html/PS4.php">Accueil PS4</a></li>
-                        <li><a href="../html/aventurePS4.php">Aventure</a></li>
-                        <li><a href="../html/actionPS4.php">Action</a></li>
-                        <li><a href="../html/combatPS4.php">Combat</a></li>
-                        <li><a href="../html/sportPS4.php">Sport</a></li>
+                        <li><a href="#">Aventure</a></li>
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Combat</a></li>
+                        <li><a href="#">Sport</a></li>
                     </ul>
                 </li>
 
@@ -48,18 +48,17 @@ if(isset($_SESSION['id']) AND !empty(isset($_SESSION['id'])))
                     <a data-toggle="dropdown" href="#">XBOX ONE<b class="caret"></b></a>
                     <ul class="dropdown-menu jqueryFadeIn">
                         <li><a href="../html/XboxONE.php">Accueil XBOX ONE</a></li>
-                        <li><a href="../html/aventureXboxOne.php">Aventure</a></li>
-                        <li><a href="../html/actionXboxOne.php">Action</a></li>
-                        <li><a href="../html/combatXboxOne.php">Combat</a></li>
-                        <li><a href="../html/sportXboxOne.php">Sport</a></li>
+                        <li><a href="#">Aventure</a></li>
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#"">Combat</a></li>
+                        <li><a href="#">Sport</a></li>
                     </ul>
                 </li>
-
             </ul>
-            <a href="../html/index.php" title="Vers la page d'accueil"> <img id="logo" src="../img/Logo.png" alt="Logo du site"/></a>
+            <a href="../html/index.php" title="Vers la page d'accueil"> <img id="logo" src="../img/Logo.png" alt="Logo du site"/> </a>
 
             <form class="navbar-form pull-right">
-                <p class="Welcome"> Bienvenue, <?= $_SESSION['pseudo']; ?> </p>
+                <p class="Welcome"> Bienvenue <?= $_SESSION['pseudo']; ?> </p>
                 <button id="btninsciption" type="button"> <a href="./profil.php?id=<?= $_SESSION['id']; ?>">Profil </a></button>
                 <button id="btnconnexion" type="button"> <a href="./deconnexion.php"> Deconnexion </a> </button>
             </form>
@@ -72,60 +71,11 @@ if(isset($_SESSION['id']) AND !empty(isset($_SESSION['id'])))
     </div>
     <!--Fin Nav bar-->
 
-    <div class="collapse navbar-collapse" id="main-menu" style="margin-bottom: 0;">
-        <ul class="nav navbar-nav">
-            <li class="dropdown">
-                <a href="../html/index.php">Accueil<b></b></a>
-            </li>
-            <li class="dropdown">
-                <a data-toggle="dropdown" href="#">PC<b class="caret"></b></a>
-                <ul class="dropdown-menu jqueryFadeIn">
-                    <li><a href="../html/PC.php">Accueil PC</a></li>
-                    <li><a href="../html/jeux.php">Aventure</a></li>
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Combat</a></li>
-                    <li><a href="#">Sport</a></li>
-                </ul>
-            </li>
-
-            <li class="dropdown">
-                <a data-toggle="dropdown" href="#">PS4<b class="caret"></b></a>
-                <ul class="dropdown-menu jqueryFadeIn">
-                    <li><a href="../html/PS4.php">Accueil PS4</a></li>
-                    <li><a href="#">Aventure</a></li>
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Combat</a></li>
-                    <li><a href="#">Sport</a></li>
-                </ul>
-            </li>
-
-            <li class="dropdown">
-                <a data-toggle="dropdown" href="#">XBOX ONE<b class="caret"></b></a>
-                <ul class="dropdown-menu jqueryFadeIn">
-                    <li><a href="../html/XboxONE.php">Accueil XBOX ONE</a></li>
-                    <li><a href="#">Aventure</a></li>
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#"">Combat</a></li>
-                    <li><a href="#">Sport</a></li>
-                </ul>
-            </li>
-
-    <?php
-
-        <form class="navbar-form pull-right">
-           <p class="Welcome"> Bienvenue <?= $_SESSION['pseudo']; ?> </p>
-            <button id="btninsciption" type="button"> <a href="./profil.php?id=<?= $_SESSION['id']; ?>">Profil </a></button>
-            <button id="btnconnexion" type="button"> <a href="./deconnexion.php"> Deconnexion </a> </button>
-        </form>
-    </div>
-<<<<<<< HEAD
-    <!--Fin Nav bar-->
-
     <script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
         <script type="text/javascript">
-        /*début de la nav bar*/
+
     $(function() {
         // Affichage du sous menu en douceur
         jQuery('ul.nav li.dropdown').hover(function() {
@@ -135,33 +85,8 @@ if(isset($_SESSION['id']) AND !empty(isset($_SESSION['id'])))
         });
 
     });
-    /* fin de la nav bar */
+
     </script>
-=======
-</nav>
-
-<div class="jumbotron">
-    <h1>GamingKeys !</h1>
-    <p> Bienvenue sur GamingKeys.</p>
-</div>
-<!--Fin Nav bar-->
-
-        <script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
-        <script type="text/javascript">
-
-            $(function() {
-                // Affichage du sous menu en douceur
-                jQuery('ul.nav li.dropdown').hover(function() {
-                    jQuery(this).find('.jqueryFadeIn').stop(true, true).delay(200).fadeIn();
-                }, function() {
-                    jQuery(this).find('.jqueryFadeIn').stop(true, true).delay(200).fadeOut();
-                });
-
-            });
-
-        </script>
 
 
         <?php
@@ -237,7 +162,6 @@ if(isset($_SESSION['id']) AND !empty(isset($_SESSION['id'])))
     <p> Bienvenue sur GamingKeys.</p>
 </div>
 <!--Fin Nav bar-->
->>>>>>> master
 
         <script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
