25eeb132645aca8797fd1ccbf3957b8a038f8fa5
Thomas <thomashtl33@gmail.com>
2018-04-17 14:16:11 +0200

    Pull

--- Site/html/dbconnect.php
+++ Site/html/dbconnect.php
@@ -2,7 +2,7 @@
 
 //Connexion à la base de données
 try {
-    $bdd = new PDO('mysql:host=localhost;dbname=ecommerce_espace_membre', 'root', 'root');
+    $bdd = new PDO('mysql:host=localhost;dbname=ecommerce_espace_membre', 'root', '');
 
 } catch (Exception $e) {
     die('Erreur : ' .$e->getMessage());
